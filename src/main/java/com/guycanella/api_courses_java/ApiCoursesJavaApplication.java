cce1e37efd60478e4730649c3fc72ba27692f227
Guilherme Canella <guycanella@gmail.com>
2025-08-27 17:32:25 -0300

    download project from spring initializr

---
+++ src/main/java/com/guycanella/api_courses_java/ApiCoursesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.guycanella.api_courses_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCoursesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCoursesJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guycanella/api_courses_java/ApiCoursesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guycanella.api_courses_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCoursesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
